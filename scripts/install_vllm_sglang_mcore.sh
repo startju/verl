0176a0627debb7d3f38a90c64f389113000380e1
startju <zxylvlp@gmail.com>
2026-04-23 14:42:19 +0800

    init env

--- scripts/install_vllm_sglang_mcore.sh
+++ scripts/install_vllm_sglang_mcore.sh
@@ -3,13 +3,13 @@
 USE_MEGATRON=${USE_MEGATRON:-1}
 USE_SGLANG=${USE_SGLANG:-1}
 
-export MAX_JOBS=32
+export MAX_JOBS=4
 
 echo "1. install inference frameworks and pytorch they need"
 if [ $USE_SGLANG -eq 1 ]; then
-    pip install "sglang[all]==0.5.2" --no-cache-dir && pip install torch-memory-saver --no-cache-dir
+    pip install "sglang[all]==0.5.2" -v && pip install torch-memory-saver -v
 fi
-pip install --no-cache-dir "vllm==0.11.0"
+pip install -v "vllm==0.11.0"
 
 echo "2. install basic packages"
 pip install "transformers[hf_xet]>=4.51.0" accelerate datasets peft hf-transfer \
@@ -26,30 +26,30 @@ pip install "nvidia-ml-py>=12.560.30" "fastapi[standard]>=0.115.0" "optree>=0.13
 
 echo "3. install FlashAttention and FlashInfer"
 # Install flash-attn-2.8.1 (cxx11abi=False)
-wget -nv https://github.com/Dao-AILab/flash-attention/releases/download/v2.8.1/flash_attn-2.8.1+cu12torch2.8cxx11abiFALSE-cp312-cp312-linux_x86_64.whl && \
-    pip install --no-cache-dir flash_attn-2.8.1+cu12torch2.8cxx11abiFALSE-cp312-cp312-linux_x86_64.whl
+#wget -nv https://github.com/Dao-AILab/flash-attention/releases/download/v2.8.1/flash_attn-2.8.1+cu12torch2.8cxx11abiFALSE-cp312-cp312-linux_x86_64.whl && \
+    pip install  flash_attn-2.8.1+cu12torch2.8cxx11abiFALSE-cp312-cp312-linux_x86_64.whl
 
-pip install --no-cache-dir flashinfer-python==0.3.1
+pip install  flashinfer-python==0.3.1
 
 
 if [ $USE_MEGATRON -eq 1 ]; then
     echo "4. install TransformerEngine and Megatron"
     echo "Notice that TransformerEngine installation can take very long time, please be patient"
     pip install "onnxscript==0.3.1"
-    NVTE_FRAMEWORK=pytorch pip3 install --no-deps git+https://github.com/NVIDIA/TransformerEngine.git@v2.6
-    pip3 install --no-deps git+https://github.com/NVIDIA/Megatron-LM.git@core_v0.13.1
+    NVTE_FRAMEWORK=pytorch pip3 install -v --no-deps git+https://github.com/NVIDIA/TransformerEngine.git@v2.6
+    pip3 install -v --no-deps git+https://github.com/NVIDIA/Megatron-LM.git@core_v0.13.1
 fi
 
 
 echo "5. May need to fix opencv"
-pip install opencv-python
-pip install opencv-fixer && \
+pip install -v opencv-python
+pip install -v opencv-fixer && \
     python -c "from opencv_fixer import AutoFix; AutoFix()"
 
 
 if [ $USE_MEGATRON -eq 1 ]; then
     echo "6. Install cudnn python package (avoid being overridden)"
-    pip install nvidia-cudnn-cu12==9.10.2.21
+    pip install -v nvidia-cudnn-cu12==9.10.2.21
 fi
 
 echo "Successfully installed all packages"
